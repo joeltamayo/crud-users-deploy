74d8ddfd3ef3350c756b33a8eec03df7735468b7
joel tamayo <jueltamayo25@gmail.com>
2025-04-16 04:18:35 -0600

    Versión para despliegue en Railway 1.3

--- public/index.php
+++ public/index.php
@@ -42,6 +42,7 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
  * our autoloader, along with Composer's, loads our constants
  * and fires up an environment-specific bootstrapping.
  */
+echo "Index cargado correctamente"; exit;
 
 // LOAD OUR PATHS CONFIG FILE
 // This is the line that might need to be changed, depending on your folder structure.
